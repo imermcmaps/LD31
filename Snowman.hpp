165d2c706f6c6eef9bfb0862a8694222ecfd5293
Robin <imer@imer.cc>
2014-12-06 08:18:41 +0100

    Snowman returns its type

--- Snowman.hpp
+++ Snowman.hpp
@@ -8,21 +8,30 @@
 #ifndef SNOWMAN_HPP
 #define	SNOWMAN_HPP
 #include "engine/src/SpriteNode.hpp"
+#include "Constants.hpp"
 
-class Snowman: public engine::Node {
-public:
-    static engine::Node* manufacture(Json::Value& root, engine::Node* parent) ;
-    
-    explicit Snowman(engine::Scene* scene);
-    virtual ~Snowman();
-private:
-    void Initialize(float x, float y);
-    virtual void OnRemoveNode(Node* node);
-    bool m_initialized;
+class Snowman : public engine::Node {
+protected:
+
     engine::SpriteNode* m_hat;
     engine::SpriteNode* m_head;
     engine::SpriteNode* m_middle;
     engine::SpriteNode* m_bottom;
+public:
+    static engine::Node* manufacture(Json::Value& root, engine::Node* parent);
+
+    explicit Snowman(engine::Scene* scene);
+    virtual ~Snowman();
+protected:
+    void Initialize(float x, float y);
+    virtual void OnRemoveNode(Node* node);
+    bool m_initialized;
+    
+    virtual uint8_t GetType() const{
+        return NT_SNOWMAN;
+    }
+
+
 };
 
 #endif	/* SNOWMAN_HPP */
